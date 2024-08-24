0628585008cde8af5309ad907d17ebe09b1fa5e7
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-24 13:18:10 +1000

    11th stage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider, ErrorBoundary } from '@rollbar/react'; // Provider imports 'rollbar'
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -9,17 +10,26 @@ import Signup from './pages/Signup';
 import Header from './components/Header';
 import { appPaths } from './routes';
 
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_TOKEN_ACCESS,
+  environment: 'production',
+};
+
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path={appPaths.notFound()} element={<NotFound />} />
-      <Route path={appPaths.home()} element={<Home />} />
-      <Route path={appPaths.login()} element={<Login />} />
-      <Route path={appPaths.signup()} element={<Signup />} />
-    </Routes>
-    <ToastContainer />
-  </BrowserRouter>
+  <Provider config={rollbarConfig}>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={appPaths.notFound()} element={<NotFound />} />
+          <Route path={appPaths.home()} element={<Home />} />
+          <Route path={appPaths.login()} element={<Login />} />
+          <Route path={appPaths.signup()} element={<Signup />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </ErrorBoundary>
+  </Provider>
 );
 
 export default App;
