2f6f05ce9fb5fbaa914886cdfa6b6c24ef52c096
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-26 18:32:08 +1000

    render mistake

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
-const url = 'http://localhost:3000';
-const URL = process.env.NODE_ENV === 'production' ? undefined : url;
+const URL = process.env.SOCKET_IO_URL || 'http://localhost:3000';
 const socket = io(URL);
 
 export default socket;
