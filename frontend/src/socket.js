c565a08adef821a4cde7829e235b55b563f9b8ac
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-27 14:37:26 +1000

    fixing render

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production'
+  ? process.env.REACT_APP_SOCKET_URL || window.location.origin
+  : 'http://localhost:3000';
+
 const socket = io(URL);
 
 export default socket;
