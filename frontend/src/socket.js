6e1a2c293be19837801d6b584ea639ee028f86cd
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-27 15:31:29 +1000

    render 1-2

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_SOCKET_URL || window.location.origin
-  : 'http://localhost:3000';
-
+const URL = process.env.NODE_ENV === 'production' ? `https://${process.env.RENDER_EXTERNAL_URL}` : 'http://localhost:3000';
 const socket = io(URL);
 
 export default socket;
