469cdf9e48a048fcd1b4771059f4a9635e7dc42e
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-30 17:47:30 +1000

    socket-2

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production'
-  ? process.env.RENDER_INTERNAL_URL || window.location.origin
-  : 'http://localhost:3000';
+// const URL = process.env.NODE_ENV === 'production'
+//   ? process.env.RENDER_INTERNAL_URL || window.location.origin
+//   : 'http://localhost:3000';
 
 const socket = io();
 
