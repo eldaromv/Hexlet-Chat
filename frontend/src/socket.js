79f0dcd85a071acb035ab7ed2f1a34fd5fa20a77
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-30 19:36:33 +1000

    changed socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
-const socket = io(URL);
+const socket = io();
 
 export default socket;
