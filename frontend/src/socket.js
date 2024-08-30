a97780e56fb1f52296a3f858968d2bfbd9cfa96a
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-30 18:57:54 +1000

    socket5

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : '/';
-const socket = io(URL);
+const socket = io();
 
 export default socket;
