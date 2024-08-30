1337be4e60dc2c75aba28df889e95917484627f0
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-30 17:48:15 +1000

    socket3

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client';
 
-// const URL = process.env.NODE_ENV === 'production'
-//   ? process.env.RENDER_INTERNAL_URL || window.location.origin
-//   : 'http://localhost:3000';
-
 const socket = io();
 
 export default socket;
