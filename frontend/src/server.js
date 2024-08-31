e1a7c5696aa56a5f055a9b33831c69b40c6252cb
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-08-31 15:14:14 +1000

    fixed server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -14,10 +14,10 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, '..', '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
