801d86907685a035328005c8b1dcbb5b7d679887
Eldar Osmanov <eldarosmanov@eldaromv.local>
2024-09-02 07:44:36 +1000

    test5

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production'
+  ? 'https://frontend-project-12-1-hi9m.onrender.com'
+  : 'http://localhost:3000';
 const socket = io(URL);
 
 export default socket;
